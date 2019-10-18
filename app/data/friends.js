84bb1ed3da47695cfe8cfc86fb41be5618c42e4b
JHaarla <haarladesign@gmail.com>
2019-10-18 00:24:42 -0400

    initial commit - trying this again

---
+++ app/data/friends.js
@@ -0,0 +1,133 @@
+const friendsArray = [
+    {
+        name: "Ahmed",
+        photo: "https://www.cl.cam.ac.uk/~ahz22/images/profile_photo.png",
+        scores: [
+            "5",
+            "1",
+            "4",
+            "4",
+            "5",
+            "1",
+            "2",
+            "5",
+            "4",
+            "1"
+        ]
+    },
+    {
+        name: "Jacob Deming",
+        photo: "https://pbs.twimg.com/profile_images/691785039043022849/oWsy8LNR.jpg",
+        scores: [
+            "4",
+            "2",
+            "5",
+            "1",
+            "3",
+            "2",
+            "2",
+            "1",
+            "3",
+            "2"
+        ]
+    },
+    {
+        name: "Jeremiah Scanlon",
+        photo: "https://avatars2.githubusercontent.com/u/8504998?v=3&s=460",
+        scores: [
+            "5",
+            "2",
+            "2",
+            "2",
+            "4",
+            "1",
+            "3",
+            "2",
+            "5",
+            "5"
+        ]
+    },
+    {
+        name: "Louis T. Delia",
+        photo: "https://pbs.twimg.com/profile_images/639214960049000449/lNCRC-ub.jpg",
+        scores: [
+            "3",
+            "3",
+            "4",
+            "2",
+            "2",
+            "1",
+            "3",
+            "2",
+            "2",
+            "3"
+        ]
+    },
+    {
+        name: "Elvis Presley",
+        photo: "https://i.pinimg.com/originals/6a/c4/60/6ac4604809a0baabcc34788a9011c883.jpg",
+        scores: [
+            "4",
+            "3",
+            "4",
+            "1",
+            "5",
+            "2",
+            "5",
+            "3",
+            "1",
+            "4"
+        ]
+    },
+    {
+        name: "Jordan Biason",
+        photo: "https://avatars0.githubusercontent.com/u/16859670?s=460&v=4",
+        scores: [
+            "4",
+            "4",
+            "2",
+            "3",
+            "2",
+            "2",
+            "3",
+            "2",
+            "4",
+            "5"
+        ]
+    },
+    {
+        name: "Wyatt Duesenberg",
+        photo: "https://66.media.tumblr.com/f56333a0aad01d332a0ec66d7286bb84/tumblr_inline_p0ifllW3fm1ukoi7g_500.jpg",
+        scores: [
+            "4",
+            "4",
+            "3",
+            "3",
+            "2",
+            "3",
+            "4",
+            "3",
+            "1",
+            "2"
+        ]
+    },
+    {
+        name: "Jarkko Haarla",
+        photo: "https://www.dictionary.com/e/wp-content/uploads/2018/03/Grumpy-Cat.jpg",
+        scores: [
+            "5",
+            "2",
+            "4",
+            "1",
+            "3",
+            "4",
+            "3",
+            "2",
+            "2",
+            "3"
+        ]
+    }
+];
+
+// console.log(friendsArray);
+module.exports = friendsArray;
